b7358960700f6968469975734445352d142bb3f2
Wesley Kirui <kkiruiwesley@gmail.com>
2020-11-27 20:50:10 +0300

    minor edit

--- index.js
+++ index.js
@@ -29,27 +29,44 @@ db.collection('books').get().then(function(querySnapshot){
         book_info = {id: doc.id, ...doc.data()}
         data.push({id: doc.id, ...doc.data()});
 
-        console.log(doc.data());
+        // console.log(doc.data());
+        // console.log(data.length);
 
         // get data
-        var title = doc.data().title;
-        var author = doc.data().author;
-        var published = new Date((doc.data().published.seconds + doc.data().published.nanoseconds)*1000).toLocaleDateString();
-        var genre = doc.data().genre;
-        var added = new Date((doc.data().added.seconds + doc.data().added.nanoseconds)*1000).toLocaleDateString();
+        // var title = doc.data().title;
+        // var author = doc.data().author;
+        // var published = new Date((doc.data().published.seconds + doc.data().published.nanoseconds)*1000).toLocaleDateString();
+        // var genre = doc.data().genre;
+        // var added = new Date((doc.data().added.seconds + doc.data().added.nanoseconds)*1000).toLocaleDateString();
+        // var rating = doc.data().rating;
 
-        // console.log({title: title, author: author, 
-        //         published:published,
-        //         genre: genre, added: added});
-        
-
-        // display data
-        document.getElementById("title").innerHTML = "Title: " + title;
-        document.getElementById("author").innerHTML = "Author: " + author;
-        document.getElementById("year").innerHTML = "Published: " + published;
-        document.getElementById("genre").innerHTML = "Genre: " + genre;
-        document.getElementById("added").innerHTML = "Date Added: " + added;
     });
+    // add data to the DOM
+    var listPlaceHolder = document.getElementById('books-container');
+    var bookList = ''
+    for(var i=0; i< data.length; i++){
+        console.log(data[i].title);
+
+        var title = data[i].title;
+        var author = data[i].author;
+        var published = new Date((data[i].published.seconds + data[i].published.nanoseconds)*1000).toLocaleDateString();
+        var genre = data[i].genre;
+        var added = new Date((data[i].added.seconds + data[i].added.nanoseconds)*1000).toLocaleDateString();
+        var rating = data[i].rating;
+
+        // var itemList = '' //document.createElement('li');
+        bookList += '<li id="book-list">'
+        bookList += '<img src="" alt="">'
+        bookList += '<p id="title" >' + 'Title: ' + title +'</p>'
+        bookList += '<p id="author" >' + 'Author: ' + author +'</p>'
+        bookList += '<p id="year" >' + 'Published: ' + published +'</p>'
+        bookList += '<p id="genre" >' + 'Genre: ' + genre +'</p>'
+        bookList += '<p id="rating" >' + 'Rating: ' + rating +'</p>'
+        bookList += '<p id="added" >' + 'Date Added: ' + added +'</p>'
+        bookList += '</li>'
+    }
+    
+    listPlaceHolder.innerHTML = bookList;
 
 }).catch(function(error){
     console.log("Error getting documents ", error);
